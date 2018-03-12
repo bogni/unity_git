1e61bae712e28ef5cdc40a564c33a829f6e72389
bogni <bogni77@gmail.com>
2018-03-12 13:51:42 +0900

    startCoroutines

---
+++ 20180312/coroutine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coroutine : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(callFunc());
+	}
+	
+    IEnumerator callFunc()
+    {
+        while (true)
+        {
+            Debug.Log("111");
+            yield return new WaitForSeconds(5);//WaitForSeconds객체를 생성해서 반환
+            Debug.Log("222");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("333");
+    }
+
+}
